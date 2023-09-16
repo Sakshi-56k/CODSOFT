fa59e85d4ecebb6bedbfb90eb86cbf3ebfbbe657
Sakshi-56k <145036571+Sakshi-56k@users.noreply.github.com>
2023-09-16 08:58:34 -0700

    Add files via upload

---
+++ codsoft_Calculator/calculator.js
@@ -0,0 +1,25 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        const result = eval(displayValue);
+        if (Number.isNaN(result)) {
+            throw new Error('Invalid Input');
+        }
+        displayValue = result.toString();
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        displayValue = 'Error';
+        document.getElementById('display').value = displayValue;
+    }
+}
